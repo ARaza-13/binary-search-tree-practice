0f5161b131789ad1c70951807eefa73cd5047c6c
Ali Raza <99924946+ARaza-13@users.noreply.github.com>
2024-10-23 17:06:01 -0400

    Added comments for each step in the driverScript function

--- driver.js
+++ driver.js
@@ -8,13 +8,15 @@ export default class Driver {
   }
 
   driverScript() {
+    // create a binary search tree from an array of random numbers < 100
     let randomNumbers = this.generateRandomArray(10);
     let tree = new Tree(randomNumbers);
-
     console.log("Initial random array: ", randomNumbers);
 
+    // confirm that the tree is balanced
     console.log("Is the tree balanced?", tree.isBalanced());
 
+    // print out all node data elements in level, pre, post, and in-order
     console.log("Level order traversal:");
     tree.levelOrder((node) => console.log(node.data));
 
@@ -27,19 +29,24 @@ export default class Driver {
     console.log("In-order traversal:");
     tree.inOrder((node) => console.log(node.data));
 
+    // unbalance the tree by adding several numbers > 100
     tree.insert(150);
     tree.insert(200);
     tree.insert(300);
 
+    // confirm that the tree is unbalanced
     console.log(
       "Is the tree balanced after inserting large numbers?",
       tree.isBalanced(),
     );
 
+    // balance the tree by calling rebalance
     tree.rebalance();
 
+    // confirm that the tree is balanced
     console.log("Is the tree balanced after rebalancing?", tree.isBalanced());
 
+    // print out all node data elements in level, pre, post, and in-order again
     console.log("Level order traversal:");
     tree.levelOrder((node) => console.log(node.data));
 
